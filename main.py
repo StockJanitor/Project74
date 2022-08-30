9d52a80b87c14a57e019c104a5f2db645e4462e9
unknown <stockjanitor@gmail.com>
2022-08-29 23:06:20 -0700

    help

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
+import random
+# initialize object
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    random_number = random.randint(1,10)
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", year = current_year)
+
+
+
+
+
+# run app, turn on debug
+if __name__=="__main__":
+    app.run(debug=True)
